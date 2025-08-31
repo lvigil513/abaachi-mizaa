abf79e9212e9bda65c29fd9b1c8246fd7f5fddf9
lvigil513 <lvigil513@gmail.com>
2025-08-30 19:39:47 -0600

    Update course.js

--- js/course.js
+++ js/course.js
@@ -1,5 +1,6 @@
 
 
+///Check Answer
 
 function checkAnswer(question, answer) {
 
@@ -7,29 +8,97 @@ function checkAnswer(question, answer) {
     const choice = document.querySelector(`input[name="${question}"]:checked`);
     const result = document.getElementById(`result-${question}`);
 
-    if (choice == false) {
+
+    if (choice.value == "") {
+
+
+        new Audio("../assets/sounds/sound_incorrect.wav").play();
+
 
         result.style.display = "flex";
         result.textContent = "Choose an answer.";
         result.style.color = "black";
-        return;
+
     }
 
+
     if (choice.value === answer) {
+
+
+        new Audio("../assets/sounds/sound_correct.wav").play();
+
 
         result.style.display = "flex";
         result.textContent = "Correct.";
         result.style.color = "green";
-        new Audio("../assets/sounds/sound_correct.wav").play();
 
     } else {
+
+
+        new Audio("../assets/sounds/sound_incorrect.wav").play();
 
         result.style.display = "flex";
         result.textContent = "Incorrect.";
         result.style.color = "red";
-        new Audio("../assets/sounds/sound_incorrect.wav").play();
 
     }
 
 }
+
+
+
+
+// Select all inputs that should trigger output updates
+document.querySelectorAll("input[text-output]").forEach(input => {
+
+
+    const outputId = input.getAttribute("text-output");
+    const output = document.getElementById(outputId);
+    const textPlaceholder = input.getAttribute("placeholder");
+    const textPrefix = input.getAttribute("text-prefix");
+    const textSuffix = input.getAttribute("text-suffix");
+
+
+    output.textContent = "";
+
+
+    // Event listener
+    input.addEventListener("input", () => {
+
+
+        if (input.value.trim() === "" || input.value === null || input == null) {
+
+
+            output.textContent = "";
+
+        } else {
+
+
+            if (textPrefix != null) {
+
+
+                output.textContent = textPrefix + input.value;
+
+            }
+
+            if (textSuffix != null) {
+
+
+                output.textContent = input.value + textSuffix;
+
+            }
+
+
+            if (textPrefix != null && textSuffix != null) {
+
+
+                output.textContent = textPrefix + input.value + textSuffix;
+
+            }
+
+        }
+
+    });
+
+});
 

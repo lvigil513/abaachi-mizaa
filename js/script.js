3f39f318033345b908f5b23b5c305c9dc6ba7208
lvigil513 <lvigil513@gmail.com>
2025-09-01 18:11:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,77 @@
 
 
 
+///Header List
+
+const listHeader = [
+
+
+    { page: "Home", url: "index.html" },
+    { page: "Courses", url: "courses.html" },
+    { page: "Dictionary", url: "dictionary.html" },
+    { page: "Phrases", url: "phrases.html" },
+    { page: "Games", url: "games.html" },
+    { page: "Resources", url: "resources.html" },
+
+]
+
+
+
+///Loads the Header
+
+function loadHeader() {
+
+
+
+
+    const root = document.getElementById('nav-header');
+
+
+    root.innerHTML = "";
+
+
+    listHeader.forEach(item => {
+
+
+        const container = document.createElement('div');
+        const container_text = document.createElement('p');
+
+
+        container.className = "button-header";
+        container_text.className = "text-header";
+        container_text.innerHTML = item.page;
+
+
+        const newDiv = container;
+
+
+        newDiv.setAttribute("onclick", "openLink('" + item.url + "')");
+
+
+        newDiv.appendChild(container_text);
+
+
+        root.appendChild(newDiv);
+
+    });
+
+}
+
+
+///Go to Page
+
+function openLink(input) {
+
+
+    window.location.href = input;
+
+}
+
+
 //Play Audio
 
 function playAudio(url) {
+
 
     new Audio("assets/sounds/" + url).play();
 
@@ -17,11 +85,20 @@ function toggleSection(input) {
 
 
     const element = document.getElementById(input);
+    const text = document.getElementById(input + '-toggle');
 
     if (element.style.display === 'block') {
+
+
         element.style.display = 'none';
+        text.innerText = '(expand)';
+
     } else {
+
+
         element.style.display = 'block';
+        text.innerText = '(collapse)';
+
     }
 
 }
@@ -55,3 +132,14 @@ function inputText(id, out, prefix, suffix) {
     });
 
 }
+
+
+///Run Functions
+
+window.onload = function () {
+
+
+
+    loadHeader();
+
+};
